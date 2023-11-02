87a5a12823f407fc3a39c05d560e6fda8ba7c1ca
Yufei Ge <gyfuiuc@gmail.com>
2023-11-02 10:11:03 +0800

    上传笔记

--- ikcp.h
+++ ikcp.h
@@ -266,53 +266,120 @@ typedef struct IQUEUEHEAD iqueue_head;
 //=====================================================================
 struct IKCPSEG
 {
+    // 队列节点，用于将KCP segment插入到发送队列、接收队列、发送缓冲区和接收缓冲区等链表中
 	struct IQUEUEHEAD node;
+    // 会话ID，用于标识属于同一个连接的数据包。发送方和接收方的conv值必须相同
 	IUINT32 conv;
+    // 数据包的类型，根据不同的cmd值，KCP协议会采取不同的处理策略
+ 	// IKCP_CMD_PUSH	cmd: push data
+	// IKCP_CMD_ACK		cmd: ack
+	// IKCP_CMD_WASK	cmd: window probe (ask)
+	// IKCP_CMD_WINS	cmd: window size (tell)
 	IUINT32 cmd;
+    // 分段序号，当数据包被分成多个片段进行传输时，每个片段都会被赋予一个frg值
+    // 从0开始递增，表示数据的分段顺序，逆序
+    // 接收方会根据frg值对数据包进行重组
 	IUINT32 frg;
+    // 发送或者接收方的窗口大小，窗口大小用于实现流量控制和拥塞控制
 	IUINT32 wnd;
+    // 数据包的发送时间戳，时间戳用于计算RTT和超时重传
 	IUINT32 ts;
+    // 数据包的序号，用于保证有序性和可靠性
 	IUINT32 sn;
+    // una字段表示发送方期望接收到的下一个确认序号
+    // 接收方会根据una值来确定哪些数据包已经被确认，从而更新发送缓冲区
 	IUINT32 una;
+    // 数据包的实际数据大小，字节
 	IUINT32 len;
+    // 数据包的重传时间戳，当当前时间超过重传时间戳时，发送方会触发重传
 	IUINT32 resendts;
+    // 数据包的重传超时时间，rto值会根据网络状况动态调整
 	IUINT32 rto;
+    // 快速重传计数器
+    // 当接收到的最大确认序号大于待发送最小数据包序号时，fastack值会递增
+    // 当fastack值达到一定阈值时，触发快速重传
 	IUINT32 fastack;
+    // 数据包的发送次数，当xmit值超过一定阈值时，KCP会认为连接已断开
 	IUINT32 xmit;
+    // 数据包中实际的数据内容，这是一个长度可变的数组，实际长度由len字段指定
+    // 通过如下函数分配内存
+    // static IKCPSEG* ikcp_segment_new(ikcpcb *kcp, int size)
+    // {
+    // 	return (IKCPSEG*)ikcp_malloc(sizeof(IKCPSEG) + size);
+    // }
 	char data[1];
 };
-
 
 //---------------------------------------------------------------------
 // IKCPCB
 //---------------------------------------------------------------------
 struct IKCPCB
 {
+    // 会话id, mtu最大传输单元
+    // mss最大报文段长度,segment数据最大长度, 通常mss=mtu-overhead
+    // kcp当前状态
 	IUINT32 conv, mtu, mss, state;
+    // snd_una 作为发送方已发送但未确认的最小序号
+    // snd_nxt 作为发送方下一个要发送的序号
+    // rcv_nxt 作为接收方期望收到的下一个序号
 	IUINT32 snd_una, snd_nxt, rcv_nxt;
+    // ts_recent 最近一次收到数据包的时间戳
+    // ts_lastack 最近一次发送确认包的时间戳
+    // ssthresh 慢启动阈值, 用于拥塞控制算法
 	IUINT32 ts_recent, ts_lastack, ssthresh;
+    // RTT值, 加权平均RTT值, 超时重传值, 超时重传值下限
 	IINT32 rx_rttval, rx_srtt, rx_rto, rx_minrto;
+    // 发送窗口大小, 接收窗口大小, 远程窗口大小, 拥塞窗口大小(控制向外发送的字节数), 探测标志
 	IUINT32 snd_wnd, rcv_wnd, rmt_wnd, cwnd, probe;
+    // 当前时间戳, 内部更新时间间隔, 下一次刷新的时间戳, 总数据包发送次数
 	IUINT32 current, interval, ts_flush, xmit;
+    // 接收缓冲区中的数据包数量, 发送缓冲区中的数据包数量
 	IUINT32 nrcv_buf, nsnd_buf;
+    // 接收队列中的数据包数量, 发送队列中的数据包数量
 	IUINT32 nrcv_que, nsnd_que;
+    // nodelay标志, KCP状态是否已更新
 	IUINT32 nodelay, updated;
+    // 下次窗口探测的时间戳, 探测等待时间
 	IUINT32 ts_probe, probe_wait;
+    // dead_link 连接断开的阈值, 当数据包的发送次数超过此阈值时, KCP会认为连接已断开
+    // 拥塞窗口的增量
 	IUINT32 dead_link, incr;
+    // 发送队列
 	struct IQUEUEHEAD snd_queue;
-	struct IQUEUEHEAD rcv_queue;
-	struct IQUEUEHEAD snd_buf;
-	struct IQUEUEHEAD rcv_buf;
-	IUINT32 *acklist;
+	// 接收队列
+    struct IQUEUEHEAD rcv_queue;
+	// 发送缓冲区
+    struct IQUEUEHEAD snd_buf;
+	// 接收缓冲区
+    struct IQUEUEHEAD rcv_buf;
+	// 用于存储待发送的ack信息
+    // 数组中的元素成对存储, 每对元素分别表示一个序号(sn)和对应的时间戳(ts)
+    // sn = [2*i+0], ts = [2*i+1] 
+    // 在ikcp_flush函数中, KCP会根据acklist中的信息发送确认包(ACK)
+    IUINT32 *acklist;
+    // acklist中的ack数量, acklist的长度等于 ackcount*2
 	IUINT32 ackcount;
+    // acklist的内存容量
 	IUINT32 ackblock;
+    // 指向用户自定义数据的指针, 可在callback中传递特定上下文信息
 	void *user;
+    // 储存kcp协议的缓冲区, 用于构造和发送数据包
 	char *buffer;
+    // 快速重传的阈值
+    // 当一个数据包的ack被跳过的次数达到fastresend值时, 会触发快速重传
 	int fastresend;
+    // 快速重传上限, 当数据发送次数超过fastlimit将不触发快速重传
+    // 设置为0代表不设置上限
 	int fastlimit;
+    // 是否禁用拥塞窗口的标志, 是否启用流模式的标志
 	int nocwnd, stream;
+    // 日志记录的掩码
+    // 根据logmask的值, 可以选择性地记录不同类型的日志信息, 如输出日志, 输入日志, 发送日志等
 	int logmask;
+    // 指向输出回调函数的指针
+    // kcp发送数据包时会调用的回调函数, 用户需要在此函数中实现实际发送逻辑如UDP
 	int (*output)(const char *buf, int len, struct IKCPCB *kcp, void *user);
+    // 指向记录日志回调函数的指针
 	void (*writelog)(const char *log, struct IKCPCB *kcp, void *user);
 };
 
